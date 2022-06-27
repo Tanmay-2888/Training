117fe1a8c705c32f92ccd4638ea5c9603252a04f
Tanmay-2888 <103040016+Tanmay-2888@users.noreply.github.com>
2022-06-27 13:53:29 +0530

    commit

---
+++ MVC Project/WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDAL context = new ProductDAL();
+
+        public IActionResult List()
+        {
+            ViewBag.ProductList = context.GetAllProduct();
+            return View();
+        }
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(IFormCollection form)
+        {
+            Product p = new Product();
+            p.Name = form["Name"];
+            p.Price = Convert.ToSingle(form["Price"]);
+            int res = context.Save(p);
+            if (res == 1)
+                return RedirectToAction("List");
+
+            return View();
+        }
+        [HttpGet]
+
+        public IActionResult Edit(int id)
+        {
+            Product prod = context.GetProductById(id);
+            ViewBag.Name = prod.Name;
+            ViewBag.Price = prod.Price;
+            ViewBag.Id = prod.Id;
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Edit(IFormCollection form)
+        {
+            Product prod = new Product();
+            prod.Name = form["name"];
+            prod.Price = Convert.ToSingle(form["price"]);
+            prod.Id = Convert.ToInt32(form["id"]);
+            int res = context.Update(prod);
+            if (res == 1)
+                return RedirectToAction("List");
+
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            Product prod = context.GetProductById(id);
+            ViewBag.Name = prod.Name;
+            ViewBag.Price = prod.Price;
+            ViewBag.Id = prod.Id;
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteA(int id)
+        {
+            int res = context.Delete(id);
+            if (res == 1)
+            {
+                return RedirectToAction("List");
+            }
+            else
+                return View();
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MVC Project/WebApplication1/WebApplication1/Models/ProductDAL.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Models
+{
+    public class ProductDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public ProductDAL()
+        {
+            con =  new SqlConnection(Startup.ConnectionString);
+        }
+
+        public List<Product> GetAllProduct()
+        {
+            List<Product> list = new List<Product>();
+
+            string str = "select * from Product";
+            cmd = new SqlCommand(str, con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Product p = new Product();
+                    p.Id = Convert.ToInt32(dr["Id"]);
+                    p.Name = dr["Name"].ToString();
+                    p.Price = (int)Convert.ToDecimal(dr["Price"]);
+                    list.Add(p);
+
+                }
+                con.Close();
+                return list;
+            }
+            else
+            {
+                con.Close();
+                return list;
+
+            }
+
+            
+        }
+
+        public Product GetProductById(int id)
+        {
+            Product p = new Product();
+            string query = "select * from Product where id=@id";
+            cmd = new SqlCommand(@query, con);
+            cmd.Parameters.AddWithValue("id", id);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    
+                    p.Id = Convert.ToInt32(dr["Id"]);
+                    p.Name = dr["Name"].ToString();
+                    p.Price = Convert.ToSingle(dr["Price"]);
+                    
+
+                }
+                con.Close();
+                return p;
+            }
+
+            con.Close();
+            return p;
+        }
+
+        public int Save(Product prod)
+        {
+            string str = "insert into Product values(@name,@price)";
+            cmd = new SqlCommand(str, con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@name", prod.Name);
+            cmd.Parameters.AddWithValue("@price",prod.Price);
+            int res = cmd.ExecuteNonQuery();
+            con.Close();
+            return res;
+
+            
+        }
+
+        public int Update(Product prod)
+        {
+            string str = "update Product set Name=@name,Price=@price where Id=@id";
+            cmd = new SqlCommand(str, con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@id", prod.Id);
+            cmd.Parameters.AddWithValue("@name", prod.Name);
+            cmd.Parameters.AddWithValue("@price", prod.Price);
+            int res = cmd.ExecuteNonQuery();
+            con.Close();
+            return res;
+            
+        }
+
+        public int Delete(int id)
+        {
+            string str = "delete from Product where Id=@id";
+            cmd = new SqlCommand(str, con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@id", id);
+            int res = cmd.ExecuteNonQuery();
+            con.Close();
+            return res;
+
+            
+        }
+
+    }
+}
